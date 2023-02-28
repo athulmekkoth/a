a779387343e0790fa40771134201c5a8194496e3
Athul <athulmekkoth21@gmail.com>
2023-02-28 16:31:10 +0530

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from "react"
+import { ethers } from "ethers"
+import abi from "../contract/abc.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App()
+{
+ 
+    const [state,setstate]=React.useState({
+      provider:null,
+      signer:null,
+      contract:null
+    })
+    useEffect(()=>{
+      const 
+    })
+      return(
+   
+    <div className="App "> </div>
+  )
 }
-
-export default App;
--- hardhat.config.js
+++ hardhat.config.js
@@ -4,14 +4,14 @@ require("dotenv").config();
 const GOERLI_URL=process.env.GOERLI_URL
 const PRIVATE_KEY=process.env.PRIVATE_KEY
 module.exports = {
-  solidity: "0.8.17",
-  network:{
-    goerli:{
-      url:GOERLI_URL,
-      accounts:[PRIVATE_KEY]
-
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
     }
-  }
+  },
+  // ...
 };
 
 /*
